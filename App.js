acf148108f105b0b1ff527be7bd1229daae40a46
devRaissa <kaoriraissa87@gmail.com>
2025-04-17 23:10:28 -0300

    Primeiros passos

--- App.js
+++ App.js
@@ -1,11 +1,69 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { FlatList, StyleSheet, Text, View } from 'react-native';
+import Header from './src/Header';
+import List from './src/List';
+
 
 export default function App() {
+const {feed, setFeed} = useState([
+  {
+    id: '1',
+    nome: 'Raissa kaori',
+    descricao: 'Sou linda',
+    imgperfil: 'https://sujeitoprogramador.com/instareact/fotoPerfil1.png',
+    publicacao: 'https://sujeitoprogramador.com/instareact/foto1.png',
+    like: true,
+    likers: 1
+  },
+  {
+    id: '2',
+    nome: 'Raissa kaori',
+    descricao: 'Sou linda',
+    imgperfil: 'https://sujeitoprogramador.com/instareact/fotoPerfil2.png',
+    publicacao: 'https://sujeitoprogramador.com/instareact/foto2.png',
+    like: true,
+    likers: 1
+  },
+  {
+    id: '3',
+    nome: 'Raissa kaori',
+    descricao: 'Sou linda',
+    imgperfil: 'https://sujeitoprogramador.com/instareact/fotoPerfil3.png',
+    publicacao: 'https://sujeitoprogramador.com/instareact/foto3.png',
+    like: true,
+    likers: 1
+  },
+  {
+    id: '4',
+    nome: 'Raissa kaori',
+    descricao: 'Sou linda',
+    imgperfil: 'https://sujeitoprogramador.com/instareact/fotoPerfil1.png',
+    publicacao: 'https://sujeitoprogramador.com/instareact/foto4.png',
+    like: true,
+    likers: 1
+  },
+  {
+    id: '5',
+    nome: 'Raissa kaori',
+    descricao: 'Sou linda',
+    imgperfil: 'https://sujeitoprogramador.com/instareact/fotoPerfil2.png',
+    publicacao: 'https://sujeitoprogramador.com/instareact/foto5.png',
+    like: true,
+    likers: 1,
+  }
+])
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Header/>
+
+      <FlatList
+      showsVerticalScrollIndicator={false}
+      keyExtractor={ (item) => item.id }
+      data={feed}
+      renderItem={ ({ item }) => <List data={item} /> }
+      />
+
     </View>
   );
 }
@@ -13,8 +71,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#fff'
   },
-});
+})
